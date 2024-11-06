f881bc22d3b2023c58be09c62060c913641d5ecd
caozhongwei <Czw@8206794>
2024-11-06 21:36:14 +0800

    init

---
+++ src/main/java/com/example/HelloWorldSpringBoot/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.HelloWorldSpringBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";  // 返回 index.html 视图
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello";  // 返回 hello.html 视图
+    }
+}
---
+++ src/test/java/com/example/HelloWorldSpringBoot/HelloWorldSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
