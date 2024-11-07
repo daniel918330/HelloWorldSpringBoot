37b1310d3260606051d677870380e970404085d4
caozhongwei <Czw@8206794>
2024-11-07 15:05:20 +0800

    modify 10

--- src/main/java/com/example/HelloWorldSpringBoot/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldSpringBoot/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sayHello(Model model) {
-        model.addAttribute("message", "Hello, World ccc!");
+        model.addAttribute("message", "Hello, World ddd!");
         return "hello";  // 返回 hello.html 视图
     }
 }
